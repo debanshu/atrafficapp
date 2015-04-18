aecb2e9a7d7544f1c3f37517e0567651e819917c
Debanshu Sinha <sinhadebanshu@gmail.com>
2015-04-18 09:18:28 +0530

    Website first

---
+++ js/ata.js
@@ -0,0 +1,20 @@
+function handleFiles(files) {
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    var imageType = /^image\//;
+    
+    if (!imageType.test(file.type)) {
+      continue;
+    }
+    
+    var img = document.createElement("img");
+    img.classList.add("obj");
+    img.file = file;
+    var preview = document.getElementById("preview");
+    preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
+    
+    var reader = new FileReader();
+    reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+    reader.readAsDataURL(file);
+  }
+}
